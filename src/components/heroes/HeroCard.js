18f02899d1e5bf823ca5834aa3b9e207991a1cc9
Mati69lbt <Mati69_lbt@hotmail.com>
2022-08-01 18:08:06 -0300

    prueba4

--- src/components/heroes/HeroCard.js
+++ src/components/heroes/HeroCard.js
@@ -8,6 +8,9 @@ const HeroCard = ({
   first_appearance,
   characters,
 }) => {
+  var a = `./assets/heroes/${id}.jpg`;
+  console.log(a);
+
   return (
     <div
       className="card row gx-5"
@@ -19,8 +22,7 @@ const HeroCard = ({
       <div className="row g-0 col">
         <div className="col-5 col-sm-4 p-3 border bg-light">
           <img
-            // src={`../assets/heroes/${id}.jpg`}
-            src={`./public/assets/heroes/${id}.jpg`}
+            src={`./assets/heroes/${id}.jpg`}
             alt={superhero}
             className="card-img"
           />
